e3e85e1399880872a8a60a38ed1da66bfa077455
seongwon07718-spec <seongwon07718@gmail.com>
2025-10-06 19:45:10 +0900

    main.py

--- main.py
+++ main.py
@@ -23,13 +23,11 @@ client.on('ready', (client) => {
 client.on('messageCreate', async (message) => {
   if (message.content === 'ping') {
     const textComponent = new TextDisplayBuilder().setContent(
-      '자동화 로벅스(제목)\n아래 버튼을 눌러 이용해주세요\n자충 오류 문의는 [오류 문의하기](https://discord.com/channels/1419200424636055592/1423477824865439884)'
+      '**자동화 로벅스** — 아래 버튼을 눌러 이용해주세요\n자충 오류 문의는 [오류 문의하기](https://discord.com/channels/1419200424636055592/1423477824865439884)'
     );
 
     const containerComponent = new ContainerBuilder().addTextDisplayComponents(
-      textComponent,
-      textComponent,
-      textComponent
+      textComponent // 하나만 추가
     );
 
     message.channel.send({
