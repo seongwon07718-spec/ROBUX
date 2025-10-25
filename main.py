0520387629202c114756e981b16077b4987032de
seongwon07718-spec <seongwon07718@gmail.com>
2025-10-25 14:12:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,48 +12,45 @@ class MyLayoutVending(ui.LayoutView):
     def __init__(self):
         super().__init__(timeout=None)
 
-        # 여기서 c를 self.c로 변경하여 인스턴스 변수로 만듭니다
-        self.c = ui.Container(ui.TextDisplay("**24시간 OTT 자판기**\n-# 버튼을 눌러 이용해주세요 !"))
-        self.c.add_item(ui.Separator(spacing=discord.SeparatorSpacing.small))
-        self.c.add_item(ui.TextDisplay("**자동충전** 오류시 [바로가기](https://discord.com/channels/1419200424636055592/1428821675142811729)\n**제품설명** 확인은? [바로가기](https://discord.com/channels/1419200424636055592/1430852730720878695)"))
-        self.c.add_item(ui.Separator(spacing=discord.SeparatorSpacing.small))
-        
-        custom_emoji1 = PartialEmoji(name="3_", id=1426934636428394678)
-        custom_emoji2 = PartialEmoji(name="6_", id=1426943544928505886)
-        custom_emoji3 = PartialEmoji(name="5_", id=1426936503635939428)
-        custom_emoji4 = PartialEmoji(name="4_", id=1426936460149395598)
+        c = ui.Container(ui.TextDisplay("**최저가 부스트**\n-# 버튼을 눌러 이용해주세요"))
+        c.add_item(ui.Separator(spacing=discord.SeparatorSpacing.small))
 
-        # 버튼 생성 및 이모지 추가
+
+        boost = ui.Section(ui.TextDisplay("**부스트 재고\n-# 60초마다 갱신됩니다**"), accessory=ui.Button(label="1000 부스트", disabled=True, style=discord.ButtonStyle.primary))
+        c.add_item(boost)
+        c.add_item(ui.Separator(spacing=discord.SeparatorSpacing.small))
+        
+        custom_emoji1 = PartialEmoji(name="d3", id=1431500583415713812)
+        custom_emoji2 = PartialEmoji(name="d8", id=1431500580198682676)
+        custom_emoji3 = PartialEmoji(name="d4", id=1431500582295965776)
+        custom_emoji4 = PartialEmoji(name="d19", id=1431500579162554511)
+
         button_1 = ui.Button(label="충전", custom_id="button_1", emoji=custom_emoji1)
-        button_2 = ui.Button(label="알림", custom_id="button_2", emoji=custom_emoji2)
+        button_2 = ui.Button(label="후기", custom_id="button_2", emoji=custom_emoji2)
         button_3 = ui.Button(label="정보", custom_id="button_3", emoji=custom_emoji3)
         button_4 = ui.Button(label="구매", custom_id="button_4", emoji=custom_emoji4)
         
-        # 버튼 행 생성
         linha = ui.ActionRow(button_1, button_2)
         linha2 = ui.ActionRow(button_3, button_4)
         
-        # 컨테이너에 버튼 행 추가
-        self.c.add_item(linha)
-        self.c.add_item(linha2)
-        
-        # 뷰에 컨테이너 추가
-        self.add_item(self.c)
+        c.add_item(linha)
+        c.add_item(linha2)
+        self.add_item(c)
 
-@bot.tree.command(name="자판기패널", description="자판기 패널을 표시합니다")
+@bot.tree.command(name="부스트_자판기", description="부스트 자판기를 표시합니다")
 @app_commands.checks.has_permissions(administrator=True)
 async def panel_vending(interaction: discord.Interaction):
-    # 레이아웃 뷰 사용하기
+
     view = MyLayoutVending()
-    await interaction.response.send_message("자판기 패널이 생성되었습니다.", view=view, ephemeral=False)
+    await interaction.response.send_message(view=view, ephemeral=False)
 
 @bot.event
 async def on_ready():
     try:
         synced = await bot.tree.sync()
-        print(f"{len(synced)}개의 슬래시 명령이 동기화되었습니다.")
+        print(f"✅{len(synced)}개의 슬래시 명령이 동기화 완료")
     except Exception as e:
-        print("명령 동기화 오류:", e)
-    print(f"{bot.user}로 로그인했습니다.")
+        print("❌명령 동기화 오류:", e)
+    print(f"✅{bot.user}로 로그인 성공")
 
 bot.run("")
