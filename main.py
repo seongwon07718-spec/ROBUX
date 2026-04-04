6d96c4be1172f7b1e58f5a85f7d18a15ac7cb1a7
seongwon07718-spec <seongwon07718@gmail.com>
2026-04-05 04:10:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-if __name__ == "__main__":
-    import sqlite3
-    with sqlite3.connect("robux_shop.db") as conn:
-        cur = conn.cursor()
-        cur.execute("SELECT value FROM config WHERE key = 'roblox_cookie'")
-        row = cur.fetchone()
-    
-    api = RobloxAPI(row[0])
-    result = api.buy_gamepass(113083961)
-    print(f"결과: {result}")
+[구매결과] status=200 body={"purchased":false,"reason":"ApplicationError","statusCode":500,"title":"Error","errorMsg":"An error occured while processing this transaction. No Robux have been removed from your account. Please try again later.","showDivId":"TransactionFailureView","shortfallPrice":-41,"balanceAfterSale":41,"expectedPrice":5,"currency":1,"price":0}
+결과: {'purchased': False, 'reason': 'ApplicationError', 'statusCode': 500, 'title': 'Error', 'errorMsg': 'An error occured while processing this transaction. No Robux have been removed from your account. Please try again later.', 'showDivId': 'TransactionFailureView', 'shortfallPrice': -41, 'balanceAfterSale': 41, 'expectedPrice': 5, 'currency': 1, 'price': 0}
