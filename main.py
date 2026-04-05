ebddf6c427271406348016c28ceef5c63b61948b
seongwon07718-spec <seongwon07718@gmail.com>
2026-04-05 10:08:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-(async () => {
-  const db = await new Promise((resolve, reject) => {
-    const req = indexedDB.open("hbaStore");
-    req.onsuccess = e => resolve(e.target.result);
-    req.onerror = reject;
-  });
-  const tx = db.transaction("hbaObjectStore", "readonly");
-  const store = tx.objectStore("hbaObjectStore");
-  const req = store.get("hba_keys");
-  req.onsuccess = async (e) => {
-    const keys = e.target.result;
-    const exported = await crypto.subtle.exportKey("pkcs8", keys.privateKey);
-    const b64 = btoa(String.fromCharCode(...new Uint8Array(exported)));
-    console.log("PRIVATE_KEY=" + b64);
-  };
-})();
+indexedDB.databases().then(dbs => console.log(JSON.stringify(dbs)))
