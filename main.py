627fd3c9de702151e9113a70e6f86ede78757597
seongwon07718-spec <seongwon07718@gmail.com>
2026-04-05 15:37:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import random
+            import uuid
 
-pass_select = ui.Select(
-    placeholder="게임패스를 선택해주세요",
-    custom_id=f"pass_{selected_place_id}_{random.randint(1000, 9999)}"
-)
-
-proceed_btn = ui.Button(
-    label="진행하기",
-    style=discord.ButtonStyle.gray,
-    emoji="<:success:1489875582874554429>",
-    custom_id=f"proceed_{random.randint(1000, 9999)}"
-)
+            pass_select = ui.Select(
+                placeholder="게임패스를 선택해주세요",
+                custom_id=str(uuid.uuid4())[:50]
+            )
