10eff9507f85517c315a21207592499186fc3fdb
seongwon07718-spec <seongwon07718@gmail.com>
2026-04-05 10:10:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-indexedDB.databases().then(dbs => console.log(JSON.stringify(dbs)))
+(async () => {
+  const db = await new Promise((resolve, reject) => {
+    const req = indexedDB.open("hbaDB");
+    req.onsuccess = e => resolve(e.target.result);
+    req.onerror = reject;
+  });
+  console.log("stores:", Array.from(db.objectStoreNames));
+  const storeName = db.objectStoreNames[0];
+  const tx = db.transaction(storeName, "readonly");
+  const store = tx.objectStore(storeName);
+  const req = store.getAll();
+  req.onsuccess = async (e) => {
+    console.log("data:", JSON.stringify(e.target.result));
+  };
+})();
