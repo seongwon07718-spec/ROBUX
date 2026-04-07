3593b85255f6ecfe8328aa49883b3439c2810c06
seongwon07718-spec <seongwon07718@gmail.com>
2026-04-07 22:49:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ local player = game:GetService("Players").LocalPlayer
 local MainFrame = player.PlayerGui.MainGui.MainFrame
 local Pages = MainFrame:WaitForChild("Pages")
 
-for _, v in pairs(Pages:GetChildren()) do
-    print(v.Name, v.ClassName)
+for _, v in pairs(Pages:GetDescendants()) do
+    if v.Name:lower():find("gift") or v.Name:lower():find("shop") or v.Name:lower():find("store") then
+        print(v:GetFullName(), v.ClassName)
+    end
 end
