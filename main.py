0f4740e1c462047dfd9bb884dee6d48f380b283a
seongwon07718-spec <seongwon07718@gmail.com>
2026-04-07 19:19:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import os
-
-@web_app.get("/purchase-log")
-async def purchase_log_page():
-    try:
-        # main.py 와 같은 폴더에서 찾기
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        html_path = os.path.join(base_dir, "purchase_log.html")
-        with open(html_path, "r", encoding="utf-8") as f:
-            return HTMLResponse(f.read())
-    except Exception as e:
-        return HTMLResponse(f"<h1>오류: {e}</h1>")
+curl : {"detail":"Not Found"}
+위치 줄:1 문자:1
++ curl http://localhost:8080/purchase-log
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : InvalidOperation: (System.Net.HttpWebRequest:HttpWebRequest) [Invoke-WebRequest], WebException
+    + FullyQualifiedErrorId : WebCmdletWebResponseException,Microsoft.PowerShell.Commands.InvokeWebRequestCommand
