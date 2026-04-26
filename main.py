aa370b99b00ba3a4354d7ce4a37a43eaa88b1312
seongwon07718-spec <seongwon07718@gmail.com>
2026-04-26 09:24:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-tunnel: ott-server
-credentials-file: ~/.cloudflared/<터널ID>.json
+tunnel: ac786224-2c27-4e81-b029-55dd0b047370
+credentials-file: C:\Users\user\.cloudflared\ac786224-2c27-4e81-b029-55dd0b047370.json
 
 ingress:
-  - hostname: 도메인.com
+  - hostname: 여기에도메인.com
     service: http://localhost:8000
   - service: http_status:404
